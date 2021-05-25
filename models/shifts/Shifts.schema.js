97a4edd5d14bd5f961be81909fc20ef3020580e1
puja bhandari <puzabhandari74.20@gmail.com>
2021-05-25 19:44:01 +1000

    checklist bug fix

--- models/shifts/Shifts.schema.js
+++ models/shifts/Shifts.schema.js
@@ -1,21 +1,52 @@
 import mongoose from 'mongoose'
 
+
 const ShiftSchema = mongoose.Schema({
-    name:{
-        type: String,
-        require: true,
-        default: "",
-    },
+
     date: {
-        type: Date,
-        require: true,
-        default: Date.now()
-    },
-    time: {
-        type: String,
-        require: true,
-        default:  "5:00"
-    },
+            type: Date,
+            require: true,
+            default: Date.now()
+        },
+    shift:[{
+        name: {
+                type: String,
+                require: true,
+                default: "",
+            },
+        eId: {
+            type: mongoose.Schema.Types.ObjectId,
+            require: true,
+            default: null,
+
+        },
+        startTime:  {
+                type: String,
+                require: true,
+                default:  "5:00"
+            },
+        endTime:  {
+                type: String,
+                require: true,
+                default:  "5:00"
+            },
+
+    }]
+    // name:{
+    //     type: Array,
+    //     require: true,
+    //     default: [],
+    // },
+    // date: {
+    //     type: Date,
+    //     require: true,
+    //     default: Date.now()
+    // },
+    // time: {
+    //     type: String,
+    //     require: true,
+    //     default:  "5:00"
+    // },
 },
 {
     timestamp: true,
